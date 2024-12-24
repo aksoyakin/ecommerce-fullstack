dc81e593f6ddb29116380474157efeb81b2a3015
AKIN AKSOY <dev@aksoyakin.com>
2024-12-24 07:52:04 +0300

    product crud completed

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -49,17 +49,36 @@ const addProduct = async (req, res) => {
 
 // List product
 const listProducts = async (req, res) => {
-
+    try {
+        const products = await productModel.find({});
+        res.json({success: true, products});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
 }
 
 // Remove product
 const removeProduct = async (req, res) => {
-
+    try {
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({success: true, message: "Product removed successfully"});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
 }
 
 // Single product info
 const singleProduct = async (req, res) => {
-
+    try {
+        const { productId } = req.body;
+        const product = await productModel.findById(productId);
+        res.json({success: true, product});
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
 }
 
 export { listProducts, addProduct, removeProduct, singleProduct };
